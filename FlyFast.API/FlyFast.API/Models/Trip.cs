0e5a9046c8c851dc018832ee3e239ee2f52a76e5
vladnelson <didimilca@hotmail.fr>
2021-09-20 09:36:10 +0200

    Add Model

---
+++ FlyFast.API/FlyFast.API/Models/Trip.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FlyFast.API.Models
+{
+    public class Trip
+    {        
+        public AIRPORT Departure { get; set; }
+        public AIRPORT Arrived { get; set; }
+        public DateTime Date { get; set; }
+        public double Price { get; set; }
+        public Plane Plane { get; set; }
+
+    }
+}
